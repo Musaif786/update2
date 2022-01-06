78e66b67433d2352d2a08425f7ba7f043c762ce2
Musaif <mdmmusaif.mm@gmail.com>
2022-01-06 18:19:27 +0530

    animation and page effect added

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,7 +3,33 @@ import "../css/Marquee.css";
 
 const Marquee=()=>{
     return(<>
-    <marquee behavior="scroll" scrollamount="10" direction="left">what are the project you have been seen all done by mohammed zayed || our Review you can check it on instagram or else on facebook</marquee>
+    <marquee behavior="scroll" scrollamount="10" direction="">what are the project you have been seen all done by mohammed zayed || our Review you can check it on instagram or else on facebook</marquee>
+
+    {/* <marquee  behavior="scroll" scrollamount="10" direction="left">
+
+    <div id="marquee-box">
+     <div className="m-1"> <img src="https://source.unsplash.com/200x200/?nature,water
+" alt="" /> </div>
+     <div className="m-2"> <img src="https://source.unsplash.com/200x200/?nature,water
+" alt="" /> </div>
+     <div className="m-3"> <img src="https://source.unsplash.com/200x200/?nature,water
+" alt="" /> </div>
+     <div className="m-4"> <img src="https://source.unsplash.com/200x200/?nature,water
+" alt="" /> </div>
+     <div className="m-5"> <img src="https://source.unsplash.com/200x200/?nature,water
+" alt="" /> </div>
+     <div className="m-6"> <img src="https://source.unsplash.com/200x200/?nature,water
+" alt="" /> </div>
+     <div className="m-7"> <img src="https://source.unsplash.com/200x200/?nature,water
+" alt="" /> </div>
+     <div className="m-8"> <img src="https://source.unsplash.com/200x200/?nature,water
+" alt="" /> </div>
+     {/* <div className="m-7"> img9 </div>
+     <div className="m-8"> img10 </div>
+     <div className="m-7"> img11 </div>
+     <div className="m-8"> img12 </div> 
+    </div>
+    </marquee> */}
     </>)
 }
 export default Marquee;
--- src/components/PhotosBox.jsx
+++ src/components/PhotosBox.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect } from "react";
 import "../css/PhotosBox.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 
  const PhotosBox = ()=>{ 
+
+     useEffect(()=>{
+          Aos.init({ duration:200});
+     },[]);
+     
 const btns = ()=>{
    
 //    var p = document.querySelector('#btn-box');
@@ -20,6 +27,7 @@ const btns = ()=>{
 // imgs.classList.toggle("img-zoom");
 
   }
+  //img zoom function
   useEffect(()=>{
        const a = document.querySelectorAll("img");
    const num = 0;
@@ -44,15 +52,46 @@ const btns = ()=>{
     
 //     }
 var w = window.innerWidth;
+var num=0;
+var box =document.getElementsByClassName("PhotosBox");
+var PBNext =()=>{
+   
+     num++;
+     for(let i=0; i<box.length;i++){
+          box[i].style.display="none";
+     }
+     if(num>=box.length){
+          num=0;
+      }
+     
+      box[num].style.display = "block";
+  
+
+}
 
 
-     return(<><div className="PhotosBox">
+function PBBack(){
+     num--;
+     for(let i=box.length-1; i>=0; i--){
+box[i].style.display ="none";
+     }
+     if(num <0){
+        num=box.length-1;
+     }
+
+     box[num].style.display = "block";
+ }
+
+
+     return(<><div className="wrapper">
+
+     <div id="p-b-first" className="PhotosBox">
      <div className="center-box">
 
         
          {/* c1 */}
 
-         <div className="photos-container">
+         <div data-aos="fade-right" className="photos-container">
              <h2>cupboard</h2>
              <img id="g-imgs-list" onClick={imglistclick} src="./images/cupboard1.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -71,7 +110,7 @@ var w = window.innerWidth;
          </div>
  {/* 2 */}
 
- <div className="photos-container">
+ <div data-aos="fade-left" className="photos-container">
              <h2>cupboard</h2>
              <img id="g-imgs-list" onClick={imglistclick}  src="./images/cupboard2.jpeg" alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -88,7 +127,7 @@ var w = window.innerWidth;
 
           {/* 3 */}
 
-          <div className="photos-container">
+          <div data-aos="fade-right" className="photos-container">
              <h2>cupboard</h2>
              <img src="./images/cupboard3.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -104,7 +143,7 @@ var w = window.innerWidth;
 
              {/* 4 */}
 
-         <div className="photos-container">
+         <div data-aos="fade-left" className="photos-container">
              <h2>cupboard</h2>
              <img src="./images/cupboard4.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -120,7 +159,7 @@ var w = window.innerWidth;
          </div>
           {/* 5 */}
 
-          <div className="photos-container">
+          <div data-aos="fade-right" className="photos-container">
              <h2>cupboard</h2>
              <img src="./images/cupboard5.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -137,7 +176,7 @@ var w = window.innerWidth;
 
           {/* 6 */}
 
-          <div className="photos-container">
+          <div data-aos="fade-left" className="photos-container">
              <h2>cupboard</h2>
              <img src="./images/cupboard6.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -152,7 +191,7 @@ var w = window.innerWidth;
         </div>
           {/* 7 same change to 8 */}
 
-          <div className="photos-container">
+          <div data-aos="fade-right" className="photos-container">
              <h2>cupboard</h2>
              <img src="./images/cupboard8.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -167,7 +206,7 @@ var w = window.innerWidth;
         </div>
           {/* 9 */}
 
-          <div className="photos-container">
+          <div data-aos="fade-left" className="photos-container">
              <h2>cupboard</h2>
              <img src="./images/cupboard9.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -183,7 +222,7 @@ var w = window.innerWidth;
 
           {/* 10 */}
 
-          <div className="photos-container">
+          <div data-aos="fade-right" className="photos-container">
              <h2>cupboard</h2>
              <img src="./images/cupboard10.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -199,7 +238,7 @@ var w = window.innerWidth;
 
           {/* 11 */}
 
-          <div className="photos-container">
+          <div data-aos="fade-left" className="photos-container">
              <h2>cupboard</h2>
              <img src="./images/cupboard11.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -217,7 +256,7 @@ var w = window.innerWidth;
 
           {/* 2 */}
 
-          <div className="photos-container">
+          <div data-aos="fade-right" className="photos-container">
              <h2>Bed</h2>
              <img src="./images/bed2.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -234,7 +273,7 @@ var w = window.innerWidth;
 
           {/* 3 */}
 
-          <div className="photos-container">
+          <div data-aos="fade-left" className="photos-container">
              <h2>Bed</h2>
              <img src="./images/bed3.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -250,7 +289,7 @@ var w = window.innerWidth;
         </div>
           {/* 4 */}
 
-          <div className="photos-container">
+          <div data-aos="fade-right" className="photos-container">
              <h2>Bed</h2>
              <img src="./images/bed4.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -266,7 +305,7 @@ var w = window.innerWidth;
         </div>
          {/* 5 */}
 
-         <div className="photos-container">
+         <div data-aos="fade-up" className="photos-container">
              <h2>Bed</h2>
              <img src="./images/bed5.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -284,7 +323,7 @@ var w = window.innerWidth;
 
               {/* 6 */}
 
-          <div className="photos-container">
+          <div data-aos="fade-left" className="photos-container">
              <h2>Bed</h2>
              <img src="./images/bed6.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -300,7 +339,7 @@ var w = window.innerWidth;
         </div>
          {/* 7 */}
 
-         <div className="photos-container">
+         <div data-aos="fade-right" className="photos-container">
              <h2>Bed</h2>
              <img src="./images/bed7.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -317,7 +356,7 @@ var w = window.innerWidth;
 
   {/* 8 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-left" className="photos-container">
              <h2>Bed</h2>
              <img src="./images/bed8.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -338,7 +377,7 @@ var w = window.innerWidth;
 
   {/* 1 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-up" className="photos-container">
              <h2>tvset</h2>
              <img src="./images/tvset1.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -355,7 +394,7 @@ var w = window.innerWidth;
         
   {/* 2 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-right" className="photos-container">
              <h2>tvset</h2>
              <img src="./images/tvset2.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -372,7 +411,7 @@ var w = window.innerWidth;
         
   {/* 3 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-left" className="photos-container">
              <h2>tvset</h2>
              <img src="./images/tvset3.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -406,7 +445,7 @@ var w = window.innerWidth;
         
   {/* 5 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-up" className="photos-container">
              <h2>tvset</h2>
              <img src="./images/tvset5.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -423,7 +462,7 @@ var w = window.innerWidth;
         
   {/* 6 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-right" className="photos-container">
              <h2>tvset</h2>
              <img src="./images/tvset6.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -440,7 +479,7 @@ var w = window.innerWidth;
         
   {/* 7 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-left" className="photos-container">
              <h2>tvset</h2>
              <img src="./images/tvset7.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -457,7 +496,7 @@ var w = window.innerWidth;
         
   {/* 8 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-right" className="photos-container">
              <h2>tvset</h2>
              <img src="./images/tvset8.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -474,7 +513,7 @@ var w = window.innerWidth;
         
   {/* 9 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-left" className="photos-container">
              <h2>tvset</h2>
              <img src="./images/tvset9.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -491,7 +530,7 @@ var w = window.innerWidth;
         
   {/* 10 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-up" className="photos-container">
              <h2>tvset</h2>
              <img src="./images/tvset10.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -509,7 +548,7 @@ var w = window.innerWidth;
   {/* kitchen */}      
   {/* 1 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-right" className="photos-container">
              <h2>kitchen</h2>
              <img src="./images/kitchen1.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -525,7 +564,7 @@ var w = window.innerWidth;
         </div>
         {/* 2 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-left" className="photos-container">
              <h2>kitchen</h2>
              <img src="./images/kitchen2.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -541,7 +580,7 @@ var w = window.innerWidth;
         </div>
         {/* 3 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-right" className="photos-container">
              <h2>kitchen</h2>
              <img src="./images/kitchen3.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -557,7 +596,7 @@ var w = window.innerWidth;
         </div>
         {/* 4 */}
 
-  <div className="photos-container">
+  <div data-aos="fade-up" className="photos-container">
              <h2>kitchen</h2>
              <img src="./images/kitchen4.jpg " alt="k4/img" />
              <h1>date: 29/wed/2021</h1>
@@ -574,7 +613,7 @@ var w = window.innerWidth;
 
 {/* 5 */}
 
-<div className="photos-container">
+<div data-aos="fade-left" className="photos-container">
              <h2>kitchen</h2>
              <img src="./images/kitchen5.jpeg " alt="" />
              <h1>date: 29/wed/2021</h1>
@@ -592,17 +631,74 @@ var w = window.innerWidth;
 
 
 
-
-
-
-
-
-
-
-
-
-
      </div>
-     </div></>);
+
+     
+        
+     </div>
+
+     {/* end */}
+     
+     {/* two box */}
+     <div className="PhotosBox">
+      {/* inside data */}
+      <div className="center-box">
+
+        
+         {/* c1 */}
+
+         <div data-aos="fade-right" className="photos-container">
+             <h2>cupboard</h2>
+             <img id="g-imgs-list" onClick={imglistclick} src="./images/cupboard1.jpeg " alt="" />
+             <h1>date: 29/wed/2021</h1>
+             <button onClick={btns} id="btn1" className="btn btn-dark px-4 my-2">More</button>
+             <div id="btn-box">
+             <p id="p1">We design furniture and related products for industrial, commercial and domestic clients balancing innovation, aesthetic appeal and functionality.we create designs for mass-produced as well as handcrafted furniture products. They must consider the function of an item of furniture, the way it looks and the limitations of the material that it is made from...
+             <a className="text-primary text-decoration-none" href="https://m.facebook.com/mohd.zayed.121?fref=nf"> <button className="btn btn-outline-primary p-0 px-3 text-primary">More</button> </a><br />
+              
+             </p>
+             <button id="btn1" onClick={btns} className="cancel-btn">
+             <i class="fa fa-times" aria-hidden="true"></i>
+
+             </button>
+             </div>
+            
+         </div>
+ {/* 2 */}
+
+ <div data-aos="fade-left"  className="photos-container">
+             <h2>cupboard</h2>
+             <img id="g-imgs-list" onClick={imglistclick}  src="./images/cupboard2.jpeg" alt="" />
+             <h1>date: 29/wed/2021</h1>
+             <button onClick={btns} id="btn1" className="btn btn-dark px-4 my-2">More</button>
+             <div id="btn-box">
+             <p id="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugiat maiores, labore unde reiciendis, ducimus aliquam illum asperiores, eligendi  amet veritatis enim unde? Ut nesciunt, libero alias doloremque sunt quo dolor ducimus quibusdam odit a vero, sit repellendus fugiat recusandae deserunt? Consequuntur placeat repellat molestiae! Veritatis odit commodi temporibus quaerat quas architecto aperiam eius autem? Quibusdam, esse!t.</p>
+             <button id="btn1" onClick={btns} className="cancel-btn">
+             <i class="fa fa-times" aria-hidden="true"></i>
+
+             </button>
+             </div>
+            
+         </div>
+         </div>
+     
+      </div>
+
+      {/* three box  more photos u can add here */}
+     {/* <div className="PhotosBox"> </div> */}
+
+
+   <div className="p-btn-box">
+   <div className="p-btn-box-center">
+
+    <button onClick={PBBack} className="p-btn-box-next"> <i class="fa fa-arrow-left" aria-hidden="true"></i>
+</button>
+    <button onClick={PBNext} className="p-btn-box-back"> <i class="fa fa-arrow-right" aria-hidden="true"></i>
+</button>
+    </div>
+   </div>
+
+    </div>
+     </>);
  }
  export default PhotosBox;
